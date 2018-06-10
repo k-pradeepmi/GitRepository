2df54964af66f7c1a88a52b8f2645a963f77a3b3
RaniDev1 <Rani@gmail.com>
2018-06-10 10:02:55 +0530

    TL- obj creaded

--- Greet/src/com/app/wish/WishTest.java
+++ Greet/src/com/app/wish/WishTest.java
@@ -1,11 +1,14 @@
 package com.app.wish;
 
+import com.app.voter.VoterEligibility;
+
 public class WishTest {
 	
 	public static void main(String[] args) {
 		WishGenerator w=new WishGenerator();
 		w.wish("Pradeep");
-		
+		VoterEligibility vote=new VoterEligibility();
+		vote.checkVoterEligibility("Pradeep", 18);
 	}
   
 }
